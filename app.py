0658e50e2f2a7ca3c5a1abf116a57f6912e2be4f
Jacques Donnelly <jacquesdonnelly@hotmail.co.uk>
2020-07-04 12:41:18 +0100

    route to cards

--- app.py
+++ app.py
@@ -20,29 +20,36 @@ ff = FlippyFlop(
     spreadsheet_id=spreadsheet_id
 )
 
-
-def get_random_card(terms):
-    random_idx = random.choice(range(len(terms)))
-    term = terms.iloc[random_idx]
-    return term["front"], term["back"]
+# TODO: Create home screen that displays before you start looking at 
+# cards and then when you have completed them all
 
 
-@app.route('/', methods=["GET", "POST"])
-def hello_world():
-    # if root then redirect to random card
+@app.route('/<card_id>', methods=["GET", "POST"])
+def single_card(card_id):
     if request.method == "GET":
         terms = ff.get_terms()
-        front, back = get_random_card(terms)
+        term = terms.loc[card_id]
+        remaining = len(terms)
+        # TODO: refactor render to bottom of function
         return render_template(
             "index.html", 
-            remaining_cards=len(terms),
-            card_front=front,
-            card_back=back
+            remaining_cards=remaining,
+            card_front=term["front"],
+            card_back=term["back"]
         )
     if request.method == "POST":
-        # if request.form["action"] == "success" then blah
-        # then redirect to next card...
-        print(request)
-        print(dir(request))
-        return "Hello, World!" 
+        # TODO: Check below works by filling in the cards on sheets 
+        success = True if request.form["action"] == "success" else False
+        ff.update_bucket(card_id, success)
+        remaining_cards = ff.todays_cards()
+        next_card = random.choice(remaining_cards)
+        terms = ff.get_terms()
+        term = terms.loc[next_card]
+        import pdb; pdb.set_trace()
+        return render_template(
+            "index.html", 
+            remaining_cards=remaining,
+            card_front=term["front"],
+            card_back=term["back"]
+        )
 
