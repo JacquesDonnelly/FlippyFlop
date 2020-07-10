fbc0768e0d47d20c35ee627c2f4e8f4daef5ba83
Jacques <47059063+JacquesDonnelly@users.noreply.github.com>
2020-07-10 11:49:16 +0100

    First Release to Digital Ocean (#1)
    * init
    
    * template
    
    * throttle _get_values
    
    * ignore google auth stuff
    
    * requirements and extract auth
    
    * add todo
    
    * Other todos
    
    * start creating app
    
    * route to cards
    
    * hack together the ui
    
    * make fast
    
    * black me up
    
    * one line todo
    
    * quizlet set to csv
    
    * DS Store no no
    
    * clean up and support newlines
    
    * finals
    
    * use prod sheet
    
    * sqlite user db and login form
    
    * bnprs
    
    * make black and add date
    
    * login required everywhere and new reqs
    
    * thank you triangolo
    
    * remove space from regex
    
    * temp app config hack
    
    * Revert "temp app config hack"
    
    This reverts commit 4c9311d65f30469ffa927d3d475d7da636fb8913.
    
    * only one worker and thres
    
    * Some thought into the refactor
    
    Co-authored-by: Jacques Donnelly <jacquesdonnelly@jacquess-air.home>

---
+++ flippyflop.py
@@ -0,0 +1,152 @@
+"""Card selection logic and read/write to google sheets"""
+
+from typing import List
+import datetime
+import time
+
+import pandas as pd
+
+# TODO: What happens if a card is deleted?
+
+
+class FlippyFlop:
+    def __init__(
+        self,
+        service,
+        spreadsheet_id,
+        throttle_time=1,
+        day_zero=datetime.datetime(2019, 12, 31),
+    ):
+        """Handle reading and writing to google sheets and daily card logic"""
+        self.service = service
+        self.spreadsheet_id = spreadsheet_id
+        # TODO: get schedule from the schedule tab
+        self.schedule = [(1, 1), (2, 2), (3, 3), (5, 4), (9, 5)]
+        self.day_zero = day_zero
+        self.time_of_last_hit = time.time()
+        self.throttle_time = throttle_time
+
+    def throttle(func):
+        """ensure api requests are at least `throttle_time` apart"""
+
+        # TODO: Should this be defined outside of class? E0213 & E1102 from pylint
+        def inner(self, *args, **kwargs):
+            time_since = time.time() - self.time_of_last_hit
+            if abs(time_since) > self.throttle_time:
+                _result = func(self, *args, **kwargs)
+                self.time_of_last_hit = time.time()
+                print(
+                    f"hitting sheets via {func.__name__} with args {args} and kwargs {kwargs}"
+                )
+            else:
+                time.sleep(0.1)
+                _result = inner(self, *args, **kwargs)
+
+            return _result
+
+        return inner
+
+    def get_terms(self) -> pd.DataFrame:
+        """All terms in the terms tab"""
+        values = self._get_values(tab="terms", cell_range="A:C")
+        terms_df = pd.DataFrame(values[1:], columns=values[0])
+        return terms_df.set_index("card_id")
+
+    def get_buckets(self) -> pd.DataFrame:
+        """Current bucket of each term"""
+        values = self._get_values(tab="buckets", cell_range="A:C")
+        bucket_df = pd.DataFrame(values[1:], columns=values[0])
+        return (
+            bucket_df.sort_values("timestamp_tested")
+            .groupby("card_id")[["bucket_after_test", "timestamp_tested"]]
+            .last()
+        )
+
+    def todays_cards(self) -> List[str]:
+        """get ids of cards left to do today"""
+        terms = self.get_terms()
+        buckets = self.get_buckets()
+        terms = terms.join(buckets)
+        today = self._todays_buckets()
+        is_in_today = terms["bucket_after_test"].isin(today)
+        earliest_today = datetime.datetime.utcnow().date().strftime("%s")
+        not_seen_today = terms["timestamp_tested"] < earliest_today
+        not_in_bucket_yet = terms["bucket_after_test"].isna()
+
+        return terms[(is_in_today & not_seen_today) | not_in_bucket_yet].index.tolist()
+
+    @throttle
+    def update_bucket(self, card_id: str, success: bool) -> None:
+        """add new row to buckets tab indicating completion of card"""
+        old_buckets = self.get_buckets()
+        if card_id in old_buckets.index:
+            old_bucket = int(old_buckets.loc[card_id]["bucket_after_test"])
+        else:
+            old_bucket = 1
+        if success:
+            change = 1
+        else:
+            change = -1
+        new_bucket = min(max(1, old_bucket + change), 5)
+
+        value_range_body = {
+            "values": [[int(card_id)], [int(time.time())], [new_bucket]],
+            "majorDimension": "COLUMNS",
+        }
+
+        (
+            self.service.spreadsheets()
+            .values()
+            .append(
+                spreadsheetId=self.spreadsheet_id,
+                range="buckets!A:C",
+                valueInputOption="RAW",
+                body=value_range_body,
+            )
+            .execute()
+        )
+
+    @throttle
+    def add_term(self, front: str, back: str) -> None:
+        """add new flash card to terms tab of google sheets
+
+        by default the id column is just incremented by 1
+        """
+        largest_card = int(self.get_terms().index.max())
+        # TODO: refactor repeated chunk of code.
+        # A single execute method to avoid need for throttle decorator
+        value_range_body = {
+            "values": [[largest_card + 1], [front], [back]],
+            "majorDimension": "COLUMNS",
+        }
+
+        (
+            self.service.spreadsheets()
+            .values()
+            .append(
+                spreadsheetId=self.spreadsheet_id,
+                range="terms!A1:D3",
+                valueInputOption="RAW",
+                body=value_range_body,
+            )
+            .execute()
+        )
+
+    def _todays_buckets(self) -> List[str]:
+        """retreive what buckets we should be reviewing today"""
+        buckets = []
+        for period, box in self.schedule:
+            if (datetime.datetime.utcnow() - self.day_zero).days % period == 0:
+                buckets.append(str(box))
+        return buckets
+
+    @throttle
+    def _get_values(self, tab: str, cell_range: str) -> List[List[str]]:
+        """generic read cell range from google sheet tab"""
+        sheet = self.service.spreadsheets()
+        result = (
+            sheet.values()
+            .get(spreadsheetId=self.spreadsheet_id, range=f"{tab}!{cell_range}")
+            .execute()
+        )
+        return result["values"]
---
+++ main.py
@@ -0,0 +1,118 @@
+"""Front end for viewing and testing a sequence of cards"""
+
+import datetime
+import os
+import pickle
+import random
+
+from flask import Flask, request, redirect, url_for, render_template, flash
+from flask_login import LoginManager, current_user, login_user, login_required
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from googleapiclient.discovery import build
+from sqlalchemy import func
+
+
+from flippyflop import FlippyFlop
+from forms import LoginForm
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "a_really_big_secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = "login"
+
+import models
+
+# TODO: Use an init / app factory + routes like miguel
+# Import here is required to import the models for Alembic (flask-migrate)
+
+# TODO: Create a config file for the whole app
+# TODO: Make file structure of app nice
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = models.User.query.filter(
+            func.lower(models.User.username) == func.lower(form.username.data)
+        ).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("home"))
+    return render_template("login.html", title="Sign In", form=form)
+
+credential_path = "./token.pickle"
+with open(credential_path, "rb") as token:
+    creds = pickle.load(token)
+
+service = build("sheets", "v4", credentials=creds)
+spreadsheet_id = "1eZL2eOCFKxGkg7bYaEmp-urWqWBfUNx73n_1oR2RkpM"
+
+ff = FlippyFlop(
+    service=service,
+    spreadsheet_id=spreadsheet_id,
+    day_zero=datetime.datetime(2020, 7, 7),
+    throttle_time=0,
+)
+
+# TODO: refactor / and /<card_id> to be more supportive of multiple users
+# Use daemon thread to execute the ff.update_bucket request 
+# Use cookies to store cards still to do today
+# Only hit ff.get_terms once on / route, then store in db rather than global
+
+
+REMAINING = ff.todays_cards()
+TERMS = ff.get_terms()
+
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
+def home():
+    """view before/after reviewing cards"""
+    global TERMS
+    global REMAINING
+    # TODO: todays_cards queries the terms tab. This happens again in get_terms.
+    if request.method == "GET":
+        REMAINING = ff.todays_cards()
+        TERMS = ff.get_terms()
+        return render_template("home.html", remaining=len(REMAINING))
+    if request.method == "POST":
+        if request.form["action"] == "start":
+            next_card = random.choice(REMAINING)
+            return redirect(f"/{next_card}")
+
+
+@app.route("/<card_id>", methods=["GET", "POST"])
+@login_required
+def single_card(card_id):
+    """view of single card with correct/incorrect buttons"""
+    global TERMS
+    global REMAINING
+    if request.method == "GET":
+        term = TERMS.loc[card_id]
+        return render_template(
+            "card.html",
+            remaining_cards=len(REMAINING),
+            card_front=term["front"].replace("\n", "<br>"),
+            card_back=term["back"].replace("\n", "<br>"),
+        )
+    if request.method == "POST":
+        success = request.form["action"] == "success"
+        ff.update_bucket(card_id, success)
+        REMAINING.remove(card_id)
+        if REMAINING:
+            next_card = random.choice(REMAINING)
+            return redirect(f"/{next_card}")
+        else:
+            return redirect("/")
