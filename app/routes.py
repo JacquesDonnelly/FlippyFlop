ffaa238124f93bdeb2fe07342635babba3d57e17
Jacques <47059063+JacquesDonnelly@users.noreply.github.com>
2020-07-18 08:44:35 +0100

    Support Multiple Workers (#3)
    * cookies now store the cards that are left
    
    * nicer names
    
    * cards should be stored in db
    
    * use TermService in root
    
    * black
    
    * part of todo is now done
    
    * Actually change the number of workers
    
    * make test import work

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,26 @@
 import random
 
-from flask import redirect, request, flash, render_template, url_for
+from flask import redirect, request, flash, render_template, url_for, make_response
 from flask_login import current_user, login_user, login_required
 from sqlalchemy import func
 
 from app import models, app
 from app.base import HeaderLinkState
 from app.insults import insult_generator
-from app.service import ff, REMAINING, TERMS
+from app.service import ff, RemainingCards, TermService
+from app.models import Card
 
 # TODO: Create derrivative of render_template to better handle base requirements.
 # For example, every render template requires a HeaderLinkState
 # There may be a nice way of automating this when registering route with decorator
+# This will also help with breaking down the content in each route
 
 # TODO: refactor / and /<card_id> to be more supportive of multiple users
 # Use daemon thread to execute the ff.update_bucket request
-# Use cookies to store cards still to do today
-# Only hit ff.get_terms once on / route, then store in db rather than global
 # Also should the get_terms and get_buckets on / route be blocking?
 # ALTERNATIVE: We want to add some reporting. Maybe implement rq with db like miguel?
 
-# TODO: Use url_for everywhere
+# TODO: Use url_for everywhere in the app
 
 
 def generate_remaining_cards_phrase(num_remaining_cards):
@@ -58,50 +58,59 @@ def login():
 @login_required
 def home():
     """view before/after reviewing cards"""
-    global TERMS
-    global REMAINING
     # TODO: todays_cards queries the terms tab. This happens again in get_terms.
     if request.method == "GET":
-        REMAINING = ff.todays_cards()
-        TERMS = ff.get_terms()
-        phrase = generate_remaining_cards_phrase(len(REMAINING))
+        remaining_cards = RemainingCards(service=ff)
+        ts = TermService(model=Card, service=ff)
+        ts.fill_database()
+        phrase = generate_remaining_cards_phrase(len(remaining_cards))
         header_link_state = HeaderLinkState(page="review")
-        return render_template(
-            "home.html",
-            remaining_cards_phrase=phrase,
-            remaining=len(REMAINING),
-            footer=" ",
-            header_link_state=header_link_state,
+        resp = make_response(
+            render_template(
+                "home.html",
+                remaining_cards_phrase=phrase,
+                remaining=len(remaining_cards),
+                footer=" ",
+                header_link_state=header_link_state,
+            )
         )
+        resp.set_cookie("cards", str(remaining_cards))
+        return resp
     if request.method == "POST":
         if request.form["action"] == "start":
-            next_card = random.choice(REMAINING)
-            return redirect(f"/{next_card}")
+            remaining_cards_cookie = request.cookies.get("cards")
+            remaining_cards = RemainingCards(string=remaining_cards_cookie)
+            next_card = remaining_cards.pop()
+            resp = make_response(redirect(f"/{next_card}"))
+            resp.set_cookie("cards", str(remaining_cards))
+            return resp
 
 
 @app.route("/<card_id>", methods=["GET", "POST"])
 @login_required
 def single_card(card_id):
     """view of single card with correct/incorrect buttons"""
-    global TERMS
-    global REMAINING
+    ts = TermService(model=Card, service=ff)
+    remaining_cards_cookie = request.cookies.get("cards")
+    remaining_cards = RemainingCards(string=remaining_cards_cookie)
     if request.method == "GET":
-        term = TERMS.loc[card_id]
+        term = ts.card_by_id(card_id)
         header_link_state = HeaderLinkState(page="review")
         return render_template(
             "card.html",
-            front=term["front"].replace("\n", "<br>"),
-            back=term["back"].replace("\n", "<br>"),
-            footer=generate_remaining_cards_phrase(len(REMAINING)),
+            front=term.front.replace("\n", "<br>"),
+            back=term.back.replace("\n", "<br>"),
+            footer=generate_remaining_cards_phrase(len(remaining_cards)),
             header_link_state=header_link_state,
         )
     if request.method == "POST":
         success = request.form["action"] == "success"
         ff.update_bucket(card_id, success)
-        REMAINING.remove(card_id)
-        if REMAINING:
-            next_card = random.choice(REMAINING)
-            return redirect(f"/{next_card}")
+        if remaining_cards:
+            next_card = remaining_cards.pop()
+            resp = make_response(redirect(f"/{next_card}"))
+            resp.set_cookie("cards", str(remaining_cards))
+            return resp
         else:
             return redirect("/")
 
--- app/service.py
+++ app/service.py
@@ -1,7 +1,12 @@
-from app.flippyflop import FlippyFlop
-import pickle
-from googleapiclient.discovery import build
 import datetime
+import pickle
+import random
+
+from googleapiclient.discovery import build
+
+from app import db
+from app.flippyflop import FlippyFlop
+
 
 credential_path = "app/auth/token.pickle"
 with open(credential_path, "rb") as token:
@@ -17,5 +22,80 @@ ff = FlippyFlop(
     throttle_time=0,
 )
 
-REMAINING = ff.todays_cards()
-TERMS = ff.get_terms()
+# TODO: A single object API for cards and terms would be better
+
+# TODO: Cards should be in DB and not google sheets
+# Below (RemainingCards and TermService)is a temp solution to enable
+# multiple workers in the app...
+# In the long run all cards will be stored in database and not on
+# Google sheets.
+
+
+class RemainingCards:
+    """Object for cards cookie"""
+
+    def __init__(self, service=None, string=None):
+        self.service = service
+        self.cards = []
+        self.delimiter = "-"
+        self.service = service
+        self.string = string
+
+        if self.string:
+            self.from_string(string)
+        elif self.service:
+            self.from_service()
+
+    def from_string(self, string):
+        self.cards = string.split(self.delimiter)
+
+    def from_service(self):
+        self.get_cards_from_service()
+        self.shuffle_cards()
+
+    def get_cards_from_service(self):
+        self.cards = self.service.todays_cards()
+
+    def shuffle_cards(self):
+        random.shuffle(self.cards)
+
+    def pop(self):
+        return self.cards.pop()
+
+    def __str__(self):
+        return self.delimiter.join(self.cards)
+
+    def __len__(self):
+        return len(self.cards)
+
+
+class TermService:
+    def __init__(self, model, service=None):
+        self.service = service
+        self.model = model
+        self.terms = None
+
+    def card_by_id(self, card_id):
+        return self.model.query.get(card_id)
+
+    def fill_database(self):
+        self.clear_db()
+        self.get_terms()
+        self.load_terms_into_db()
+
+    def get_terms(self):
+        self.terms = self.service.get_terms()
+
+    def clear_db(self):
+        self.model.query.delete()
+        db.session.commit()
+
+    def load_terms_into_db(self):
+        data = self.prepare_terms_for_db()
+        data.to_sql("card", db.engine, index=False, if_exists="append")
+
+    def prepare_terms_for_db(self):
+        data = self.terms.reset_index()
+        data.columns = ["id", "front", "back"]
+        data["id"] = data["id"].astype(int)
+        return data
