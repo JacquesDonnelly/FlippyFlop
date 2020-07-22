7b6583b54e90ae1bb43abdb0185f302b23a40689
Jacques Donnelly <jacquesdonnelly@hotmail.co.uk>
2020-07-22 08:13:27 +0100

    think about stats and tests

--- reporting/scratch.py
+++ reporting/scratch.py
@@ -3,6 +3,7 @@
 # Then a table with each row representing a bucket, columns of "Correct", "Incorrect"
 # (later) add one or two incorrect cards for ad hoc tests
 
+from typing import List
 import pickle
 import datetime
 from googleapiclient.discovery import build
@@ -23,4 +24,34 @@ ff = FlippyFlop(
     throttle_time=0,
 )
 
-buckets = ff.get_buckets()
+# TODO: BNPR. Get a method in ff to get values from buckets rather than using "private"
+values = ff._get_values(tab="buckets", cell_range="A:C")
+bucket_df = pd.DataFrame(values[1:], columns=values[0])
+bucket_df["date"] = pd.to_datetime(bucket_df["timestamp_tested"], unit="s").dt.date
+today = pd.Timestamp("2020-07-15")
+bucket_df_yest = bucket_df.query("date < @today")
+
+def correct_on_date(bucket_df: pd.DataFrame, day: pd.Timestamp) -> dict:
+    """give mapping of all cards to the results on that day
+
+    the cards that are not tested should be mapped to None, whereas correct/incorrect
+    cards should be mapped to True/False""" 
+    
+
+# TODO: BNPR. Think about a "Card" object that holds all historic bucktets. And 
+# how would we use a collection of them to calculate the stats we want below?
+
+def calculate_stats(cards_todo_on_date: List[int], result_on_date: dict) -> dict:
+    """calculate statistics for given correct on date mapping
+
+    Desired Stats:
+        Total cards required
+        Total card completed
+        Buckets completed today (we can't do this with input above yet given
+        we don't have the buckets of the cards in the args. What should we change?
+        Maybe think about a "Card" object that holds all of the historic buckets. Then
+        we can attach relevant computations to it)
+        Percentage correct per bucket
+        Percentage correct overall
+    """
+
---
+++ tests/test_reporting.py
@@ -0,0 +1,10 @@
+from reporting import correct_on_date
+
+def test_correct_on_date():
+    # 1. Create fixture that adds arbitrary cards on arbitrary dates
+    # 2. Create a test case. We want not all cards to be tested on that day and 
+    # some to be correct and some to be incorrect
+    # 3. Assertion should just be what cards were correct on what day
+
+def test_calculate_stats():
+    # 1. Think about "card" object first before writing this 
