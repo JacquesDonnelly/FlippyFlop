4a93ce30e69b599c66dff71cc537b42c61ee838e
Jacques Donnelly <jacquesdonnelly@hotmail.co.uk>
2020-07-27 08:45:28 +0100

    from df now works

--- reporting/deck.py
+++ reporting/deck.py
@@ -1,18 +1,31 @@
-from typing import Dict, Sequence
+from typing import Dict, Sequence, List
 import datetime
 from numbers import Real
 
+import pandas as pd
+
 from app.flippyflop import FlippyFlop
 
-# TODO: Think about how to create the schedule class.
-# I dont want to rely on the service.
-# Maybe just replicate the code (ff._buckets_for_timestamp) 
-# for now and revisit when creating DB?
+
+# TODO: BNPR move this to app.flippyflop and use it in FlippyFlop
 class Schedule:
     """what buckets on what day"""
 
-    def __init__(self, ff: FlippyFlop):
-        self.ff = ff
+    def __init__(self):
+        self.day_gaps = [(1, 1), (2, 2), (3, 3), (5, 4), (9, 5)]
+        self.day_zero = datetime.datetime(2019, 12, 31)
+
+    def buckets_for_timestamp(self, timestamp):
+        """retreive waht buckets we should do on a given timestamp"""
+        buckets = []
+        for period, box in self.schedule:
+            if (timestamp - self.day_zero).days % period == 0:
+                buckets.append(str(box))
+        return buckets
+
+    def todays_buckets(self) -> List[str]:
+        """retreive what buckets we should be reviewing today"""
+        return self.buckets_for_timestamp(datetime.datetime.utcnow())
 
 
 class Card:
@@ -50,17 +63,28 @@ class Deck:
     def total_incorrect_today(self) -> int:
         pass
 
+    def card_by_id(self, _id) -> Card:
+        for card in self.cards:
+            if card.id == _id:
+                return card
+
     @classmethod
-    def from_df(cls, df, schedule):
+    def from_df(cls, df, schedule=None):
         cards = []
         for card_id in df["card_id"].unique():
             card_df = df.query("card_id == @card_id")
             bucket_sequence = card_df["bucket_after_test"].astype(int)
             bucket_sequence_shifted = bucket_sequence.shift(1).fillna(1)
-            results = bucket_sequence > bucket_sequence_shifted
-            # test_dates = ** convert int times to list of datetimes **
-            cards.append(Card(_id=card_id, results=results, test_dates=test_dates))
+            results = (bucket_sequence > bucket_sequence_shifted).to_list()
+            test_dates = (
+                pd.to_datetime(card_df["timestamp_tested"], unit="s")
+                .to_list()
+            )
+            test_dates = [date.to_pydatetime() for date in test_dates]
+            cards.append(Card(_id=int(card_id), results=results, test_dates=test_dates))
 
+        if schedule is None:
+            schedule = Schedule()
         return cls(cards=cards, schedule=schedule)
 
 
--- tests/test_reporting.py
+++ tests/test_reporting.py
@@ -1,6 +1,7 @@
-from reporting.deck import Deck
+from reporting.deck import Deck, Schedule, Card
 import pytest
 from tests.fixtures import generate_dummy_service
+import datetime
 from app import flippyflop
 
 # TODO: BNPR centralize these constant values... conftest?
@@ -8,30 +9,83 @@ PROD_SPREADSHEET_ID = "1eZL2eOCFKxGkg7bYaEmp-urWqWBfUNx73n_1oR2RkpM"
 
 TEST_SPREADSHEET_ID = "1UDLGeqhVxfHJF5zk2EWRnWuQrZLQkCbwdg9loyd1nFg"
 
+
 def test_deck_from_dataframe(generate_dummy_service):
     buckets_values = [
         ["card_id", "timestamp_tested", "bucket_after_test"],
-        [1, 1578166320, 1], # "2020-01-04 19:32:00"
-        [2, 1578166444, 2], # "2020-01-04 19:34:04"
-        [3, 1578166517, 2], # "2020-01-04 19:35:17"
-        [4, 1578166560, 1], # "2020-01-04 19:36:00"
-        [5, 1578168060, 2], # "2020-01-04 20:01:00"
-        [6, 1578168060, 1], # "2020-01-04 20:01:00"
-        [1, 1578252720, 1], # "2020-01-05 19:32:00"
-        [2, 1578252844, 3], # "2020-01-05 19:34:04"
-        [4, 1578252960, 2], # "2020-01-05 19:36:00"
-        [5, 1578254460, 1]  # "2020-01-05 20:01:00"
+        [1, 1578166320, 1],  # "2020-01-04 19:32:00"
+        [2, 1578166444, 2],  # "2020-01-04 19:34:04"
+        [3, 1578166517, 2],  # "2020-01-04 19:35:17"
+        [4, 1578166560, 1],  # "2020-01-04 19:36:00"
+        [5, 1578168060, 2],  # "2020-01-04 20:01:00"
+        [6, 1578168060, 1],  # "2020-01-04 20:01:00"
+        [1, 1578252720, 1],  # "2020-01-05 19:32:00"
+        [2, 1578252844, 3],  # "2020-01-05 19:34:04"
+        [4, 1578252960, 2],  # "2020-01-05 19:36:00"
+        [5, 1578254460, 1],  # "2020-01-05 20:01:00"
     ]
-    dummy_service = generate_dummy_service(buckets_values=buckets_values, terms_values=None)
+    dummy_service = generate_dummy_service(
+        buckets_values=buckets_values, terms_values=None
+    )
     ff = flippyflop.FlippyFlop(dummy_service, TEST_SPREADSHEET_ID)
     df = ff.get_all_buckets()
     deck = Deck.from_df(df)
 
-    assert len(deck.cards) == 9 
-    assert deck.card_by_id(1).results = [False, False]
-    assert deck.card_by_id(2).results = [True, True]
-    assert deck.card_by_id(3).results = [True]
-    assert deck.card_by_id(4).results = [False, True]
-    assert deck.card_by_id(5).results = [True, False]
-    assert deck.card_by_id(6).results = [False]
-    # TODO BNPR: assert on the times also
+    assert len(deck.cards) == 6
+    assert deck.card_by_id(1).test_dates == [
+        datetime.datetime(2020,1,4,19,32),
+        datetime.datetime(2020,1,5,19,32)
+    ]
+    assert deck.card_by_id(2).results == [True, True]
+    assert deck.card_by_id(3).results == [True]
+    assert deck.card_by_id(4).results == [False, True]
+    assert deck.card_by_id(5).results == [True, False]
+    assert deck.card_by_id(6).test_dates == [
+        datetime.datetime(2020, 1, 4, 20, 1)
+    ]
+
+
+@pytest.mark.parametrize(
+    "_id,expected",
+    [
+        (
+            1,
+            {
+                "results": [True, True],
+                "test_dates": [
+                    datetime.datetime(2020, 1, 1, 8),
+                    datetime.datetime(2020, 1, 1, 9),
+                ],
+            },
+        ),
+        (
+            2,
+            {
+                "results": [False],
+                "test_dates": [
+                    datetime.datetime(2020, 1, 1, 8),
+                ],
+            },
+        ),
+    ],
+)
+def test_card_by_id(_id, expected):
+    cards = [
+        Card(
+            _id=1,
+            results=[True, True],
+            test_dates=[
+                datetime.datetime(2020, 1, 1, 8),
+                datetime.datetime(2020, 1, 1, 9),
+            ],
+        ),
+        Card(_id=2, results=[False], test_dates=[datetime.datetime(2020, 1, 1, 8),],),
+    ]
+    schedule = Schedule()
+    deck = Deck(cards, schedule)
+
+    card = deck.card_by_id(_id)
+
+    assert card.results == expected["results"]
+    assert card.test_dates == expected["test_dates"]
+
