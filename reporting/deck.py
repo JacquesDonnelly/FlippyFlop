bbe1721537247db25572e92b7a630577af893546
Jacques Donnelly <jacquesdonnelly@hotmail.co.uk>
2020-07-27 09:10:20 +0100

    refactor from_df

--- reporting/deck.py
+++ reporting/deck.py
@@ -70,22 +70,39 @@ class Deck:
 
     @classmethod
     def from_df(cls, df, schedule=None):
-        cards = []
-        for card_id in df["card_id"].unique():
-            card_df = df.query("card_id == @card_id")
-            bucket_sequence = card_df["bucket_after_test"].astype(int)
-            bucket_sequence_shifted = bucket_sequence.shift(1).fillna(1)
-            results = (bucket_sequence > bucket_sequence_shifted).to_list()
-            test_dates = (
-                pd.to_datetime(card_df["timestamp_tested"], unit="s")
-                .to_list()
-            )
-            test_dates = [date.to_pydatetime() for date in test_dates]
-            cards.append(Card(_id=int(card_id), results=results, test_dates=test_dates))
-
+        card_ids = df["card_id"].unique()
+        cards = [cls._create_card_from_df(df, card_id) for card_id in card_ids]
         if schedule is None:
             schedule = Schedule()
         return cls(cards=cards, schedule=schedule)
+
+    # TODO: BNPR: Technically now these static methods can be put somewhere else
+    # But where? Maybe in card? Just module level funcitons?
+
+    @staticmethod
+    def _create_card_from_df(df, card_id):
+        card_df = df.query("card_id == @card_id")
+        results = Deck._extract_results(card_df)
+        test_dates = Deck._extract_test_dates(card_df)
+        return Card(_id=int(card_id), results=results, test_dates=test_dates)
+
+
+    @staticmethod
+    def _extract_results(card_df):
+        bucket_sequence = card_df["bucket_after_test"].astype(int)
+        bucket_sequence_shifted = bucket_sequence.shift(1).fillna(1)
+        return (bucket_sequence > bucket_sequence_shifted).to_list()
+
+    @staticmethod
+    def _extract_test_dates(card_df):
+        # TODO: BNPR: convert straight to datetme rather than Timstamp first
+        test_dates = (
+            pd.to_datetime(card_df["timestamp_tested"], unit="s")
+            .to_list()
+        )
+        return [date.to_pydatetime() for date in test_dates]
+
+
 
 
 def gather_deck_stats(deck: Deck) -> Dict[str, Real]:
